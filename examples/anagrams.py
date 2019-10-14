0739c4cde5c159f1995a9b5a5b0eb0d6f7f552cf
Carlos Cabrera <pointtonull@users.noreply.github.com>
2019-10-14 15:36:33 +0100

    added simple examples folder

---
+++ examples/anagrams.py
@@ -0,0 +1,39 @@
+import string
+from functools import reduce
+from operator import mul
+from collections import defaultdict
+
+LETTER_PRIME = dict(
+                    zip(string.ascii_letters,
+                        (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+                         53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103 , 107,
+                         109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167,
+                         173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+                         233, 239)
+                       )
+                   )
+
+def anagram_hash(word):
+    """
+    > anagram_hash('cat')
+    710
+    > anagram_hash('tac')
+    710
+    > anagram_hash('dog')
+    5593
+    > anagram_hash('Nan' * 10 + Batman)
+    28470819877476016224161459901545001110315528646656
+    """
+    return reduce(mul, (LETTER_PRIME[c] for c in word))
+
+# Now you have a O(1) hash function you use the map approach
+# No further change is required since in python hash(integer) == integer
+
+def group_anagrams(words): # O(n)
+    groups = defaultdict(list)
+    for word in words: # O(n)
+        groups[anagram_hash(word)].append(word) # O(1)
+    return groups.values() # O(n)
+
+print(group_anagrams(["apple", "rope", "cat", "tac", "atc", "pore"]))
+
