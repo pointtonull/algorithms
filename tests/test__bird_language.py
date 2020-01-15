759fdbcdc21a44a711d76be61242411a06b09eab
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2020-01-15 03:19:55 +0000

    added bird language problem

---
+++ tests/test__bird_language.py
@@ -0,0 +1,17 @@
+from bird_language import translate
+
+
+def test__translate__signature():
+    result = translate("hieeelalaooo")
+
+    assert isinstance(result, str)
+
+
+def test__translate__examples():
+    assert translate("hieeelalaooo") == "hello", "Hi!"
+
+    assert translate("hoooowe yyyooouuu duoooiiine") == "how you doin", "Joey?"
+
+    assert translate("aaa bo cy da eee fe") == "a b c d e f", "Alphabet"
+
+    assert translate("sooooso aaaaaaaaa") == "sos aaa", "Mayday, mayday"
