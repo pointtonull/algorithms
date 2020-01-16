1487974c330493b85ff4ce889debe247cc03eec5
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2020-01-16 21:08:57 +0000

    improved final documentation

--- problem_template/tmp_{{ cookiecutter.module_name }}/src/{{ cookiecutter.module_name }}.py
+++ problem_template/tmp_{{ cookiecutter.module_name }}/src/{{ cookiecutter.module_name }}.py
@@ -1,12 +1,7 @@
-"""
-{{ cookiecutter.problem_name}}
-
-{{ cookiecutter.problem_description }}
-"""
-
 def {{ cookiecutter.function_name }}(input1, input2):
     """{{ cookiecutter.problem_name}}
 
+    {{ cookiecutter.problem_description }}
     """
     result = []
     raise NotImplementedError("Must implement {{ cookiecutter.problem_name }} solution.")
