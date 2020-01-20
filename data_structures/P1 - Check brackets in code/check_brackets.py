7710b8f95e12b2b017c22d9d1f5c357668334a8d
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2020-01-20 00:09:48 +0000

    cleaned and organized

---
+++ data_structures/P1 - Check brackets in code/check_brackets.py
@@ -0,0 +1,38 @@
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text: str) -> int:
+    opening_brackets_stack = []
+    for pos, char in enumerate(text, 1):
+        if char in "([{":
+            opening_brackets_stack.append((pos, char))
+        elif char in ")]}":
+            if not opening_brackets_stack:
+                stacked = " "
+            else:
+                _, stacked = opening_brackets_stack.pop()
+            if not are_matching(stacked, char):
+                return pos
+    else:
+        if opening_brackets_stack:
+            return opening_brackets_stack[-1][0]
+        return 0
+
+
+def main():
+    text = input()
+    mismatch = find_mismatch(text)
+    if not mismatch:
+        print("Success")
+    else:
+        print(mismatch)
+
+
+if __name__ == "__main__":
+    main()
