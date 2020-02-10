d7dfdf6890e0820884c608a7cdc2c76c1df991e7
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2020-02-10 19:46:12 +0000

    added tries problem

---
+++ src/autocomplete_system.py
@@ -0,0 +1,57 @@
+from collections import defaultdict
+
+
+class Trie:
+    def __init__(self, symbols=None):
+        self.buckets = defaultdict(Trie)
+        self.terminal = False
+        if symbols:
+            for symbol in symbols:
+                self.add(symbol)
+
+    def add(self, symbol):
+        if len(symbol) == 1:
+            self.buckets[symbol].terminal = True
+        else:
+            head, tail = symbol[0], symbol[1:]
+            self.buckets[head].add(tail)
+
+    def __repr__(self):
+        return f"Trie({list(self.buckets.keys())}..)"
+
+    def _all_leaves(self):
+        if self.terminal:
+            yield ""
+        for name, node in self.buckets.items():
+            for leave in node._all_leaves():
+                yield name + leave
+
+    def _query(self, query):
+        if query:
+            head, tail = query[0], query[1:]
+            yield from self.buckets[head]._query(tail)
+        else:
+            yield from self._all_leaves()
+
+    def query(self, query):
+        for leave in self._query(query):
+            yield query + leave
+
+
+def autocomplete_system(symbols, query):
+    """
+    AUTOCOMPLETE SYSTEM
+
+    This problem was asked by Twitter.
+    Dificulty: Medium
+
+    Implement an autocomplete system. That is, given a query string s and a set of all
+    possible query strings, return all strings in the set that have s as aprefix. For
+    example, given the query string "de" and the set of strings [dog, deer, deal],
+    return [deer, deal].
+
+    Hint: Try preprocessing the dictionary into a more efficient data structure tospeed
+    up queries.
+    """
+    trie = Trie(symbols)
+    return list(trie.query(query))
---
+++ tests/test__autocomplete_system.py
@@ -0,0 +1,72 @@
+from pytest import fixture
+
+from utils import deep_diff
+
+from src.autocomplete_system import autocomplete_system, Trie
+
+"""
+AUTOCOMPLETE SYSTEM
+
+This problem was asked by Twitter.
+Dificulty: Medium
+
+Implement an autocomplete system. That is, given a query string s and a set of all
+possible query strings, return all strings in the set that have s as aprefix. For
+example, given the query string "de" and the set of strings [dog, deer, deal], return
+[deer, deal].
+
+Hint: Try preprocessing the dictionary into a more efficient data structure tospeed
+up queries.
+"""
+
+CASES = [
+    {
+        "symbols": ["dog", "deer", "deal"],
+        "query": "de",
+        "answer": ["deer", "deal"]
+    },
+    {
+        "symbols": ["dog", "deer", "deal"],
+        "query": "",
+        "answer": ["dog", "deer", "deal"]
+    },
+    {
+        "symbols": ["dog", "deer", "deal"],
+        "query": "do",
+        "answer": ["dog"]
+    },
+]
+
+
+
+@fixture(params=CASES)
+def case(request):
+    return request.param
+
+
+def test__autocomplete_system__signature(case):
+    symbols = case["symbols"]
+    query = case["query"]
+
+    result = autocomplete_system(symbols, query)
+    assert isinstance(result, list)
+
+
+def test__autocomplete_system__examples(case):
+    symbols = case["symbols"]
+    query = case["query"]
+    answer = case["answer"]
+
+    result = autocomplete_system(symbols, query)
+
+    assert not deep_diff(answer, result, order=False)
+
+
+def test__Trie__population(case):
+    symbols = case["symbols"]
+    answer = len(symbols)
+
+    trie = Trie(symbols)
+    result = len(list(trie._all_leaves()))
+
+    assert result == answer
